6692aeb3a4b3f3f8de0c8d302d62c13e34df3fb9
Max Base <MaxBaseCode@Gmail.Com>
2019-08-24 16:07:52 +0430

    Create test1.php

---
+++ test1.php
@@ -0,0 +1,20 @@
+<?php
+function partition($transport,$sizes) {
+	$results = [];
+	foreach ($sizes as $size) {
+		$current = [];
+		foreach(range(1,$size) as $no) {
+			$mode = current($transport);
+			$current[]=$mode;
+			next($transport);
+		}
+		$results[] = array_filter($current);
+	}
+	return $results;
+}
+///////////////////////////////////////
+$input=[1,2,3,4,5,6,7];
+$size=[3,2,2];
+$output=partition($input, $size);
+print_r($output);
+sleep(500);
